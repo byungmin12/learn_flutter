e79fe257d25b8cefad3172a0519d059cb3bc821d
byungmin12 <kbm940526@naver.com>
2023-03-26 22:56:36 +0900

    데이터 페칭 부분
    실제 요청 데이터로 변경 및 main에서 데이터를 요청하는 것이 아닌 해당 컴포넌트 부분에서 요청 또한 api요청과 상태 변화를 통해 UI를 변경하는 것이 아닌 FutreBuilder 컴포넌트를 통해서 이를 해결!

--- lib/Carousel.dart
+++ lib/Carousel.dart
@@ -1,28 +1,42 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter/services/apis.dart';
 
 import 'PersonCard.dart';
+import 'models/userModel.dart';
 
 class Carousel extends StatelessWidget {
+  Carousel({super.key});
+
+  Future<List<UserModel>> users = ApiServices().getUsers();
+
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: [1, 2, 3].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return PersonCard(
-              imageUrl: 'assets/peopleImages/$i.jpg',
-              isLogin: i <= 2 ? true : false,
-            );
-          },
-        );
-      }).toList(),
-      options: CarouselOptions(
-          enlargeCenterPage: true,
-          enableInfiniteScroll: false,
-          autoPlay: false,
-          viewportFraction: 0.6,
-          height: 300),
+    return FutureBuilder(
+      builder: (cnt, snapShot) {
+        if (snapShot.hasData) {
+          return CarouselSlider(
+            items: snapShot.data?.map((user) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return PersonCard(
+                    imageUrl: user.image,
+                    isLogin: user.id <= 2 ? true : false,
+                  );
+                },
+              );
+            }).toList(),
+            options: CarouselOptions(
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
+                autoPlay: false,
+                viewportFraction: 0.6,
+                height: 300),
+          );
+        }
+        return const Text("Loading");
+      },
+      future: users,
     );
   }
 }
